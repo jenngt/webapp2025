a20b3d096232b0ab7928a13f6e1cbbcbd0baa97b
jenngt <jennlimgt@gmail.com>
2024-04-01 17:55:57 +0800

    add test button

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -2,7 +2,9 @@ const API_ENDPOINT = "https://cat-fact.herokuapp.com/facts";
 
 export default async (request, context) => {
   try {
+    console.log(123);
     let data = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+    console.log(234, data);
     return Response.json({
       request,
       context,
