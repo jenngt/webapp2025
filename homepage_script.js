25b60098f329e4e8cc0c55bcd378cf5b5902bf8c
jennlim <jennlimgt@gmail.com>
2025-01-08 17:25:18 +0800

    jenn2025

--- homepage_script.js
+++ homepage_script.js
@@ -1,7 +1,7 @@
 document.getElementById("postButton").addEventListener("click", function () {
   // Specify the URL of the new page
   const newPageUrl =
-    "https://stg-id.singpass.gov.sg/auth?scope=openid&response_type=code&redirect_uri=https://jennlimwebapp.netlify.app/callback&nonce=7863ce84-ad96-40e8-992e-09778646eb07&client_id=btKmtZ8DAU8NZlK0FSVwNayGI93ou7fO&state=dGVzdCBzdHJpbmcK"; // Replace with your desired URL
+    "https://stg-id.singpass.gov.sg/auth?scope=openid&response_type=code&redirect_uri=https://jennlimwebapp2025.netlify.app/callback&nonce=7863ce84-ad96-40e8-992e-09778646eb07&client_id=btKmtZ8DAU8NZlK0FSVwNayGI93ou7fO&state=dGVzdCBzdHJpbmcK"; // Replace with your desired URL
   // Open the new page
   window.location.href = newPageUrl;
 });
